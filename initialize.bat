1cc0a935b687271aa652f5752452fd9efa93b86f
José  Pedro <jgomes@geobit.com.br>
2020-10-21 23:44:32 -0300

    criando initialze para windows

---
+++ initialize.bat
@@ -0,0 +1,5 @@
+pip3 install -r requirements.txt
+set SECRET_KEY="UmaChaveBemSecreta"
+set FLASK_ENV="development"
+flask db init
+flask db migrate
--- initialize.sh
+++ initialize.sh
@@ -1,10 +1,5 @@
-pip3 install -r requirements.txt
-
-export SECRET_KEY="UmaChaveBemSecreta"
-set SECRET_KEY="UmaChaveBemSecreta"
-
-export FLASK_ENV="development"
-set FLASK_ENV="development"
-
-flask db init
-flask db migrate
+pip3 install -r requirements.txt 
+export SECRET_KEY="UmaChaveBemSecreta" 
+export FLASK_ENV="development" 
+flask db init 
+flask db migrate 
